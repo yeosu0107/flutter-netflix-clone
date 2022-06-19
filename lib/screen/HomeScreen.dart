b1ee4e436a5e4e157dc03c3b726bd2baaf3a7e0a
sungwoo.yeo <yeosu0107@naver.com>
2022-06-19 14:36:10 +0900

    홈화면 추가
    - 더미 무비 정보 추가
    - 무비 슬라이더 추가

--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_netflix_clone/model/Movie.dart';
+import 'package:flutter_netflix_clone/widget/CarouselSlider.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Movie> movies = [
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지1',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지2',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지3',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지4',
+      'poster': 'test_movie_1.png',
+      'like': false
+    })
+  ];
+
   @override
   void initState() {
     super.initState();
@@ -12,7 +41,13 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return TopBar();
+    return ListView(
+      children: <Widget>[
+        Stack(
+          children: <Widget>[CarouselImage(movies: movies), TopBar()],
+        )
+      ],
+    );
   }
 }
 
---
+++ lib/widget/CarouselSlider.dart
@@ -0,0 +1,119 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_netflix_clone/model/Movie.dart';
+
+class CarouselImage extends StatefulWidget {
+  final List<Movie>? movies;
+  CarouselImage({this.movies});
+
+  _CarouselImageState createState() => _CarouselImageState();
+}
+
+class _CarouselImageState extends State<CarouselImage> {
+  List<Movie>? movies;
+  List<Widget>? images;
+  List<String>? keywords;
+  List<bool>? likes;
+  int _currentPage = 0;
+  String _currentKeyword = "";
+
+  @override
+  void initState() {
+    super.initState();
+    movies = widget.movies;
+    images = movies?.map((m) => Image.asset('./images/' + m.poster)).toList();
+    keywords = movies?.map((m) => m.keyword).toList();
+    likes = movies?.map((m) => m.like).toList();
+    _currentKeyword = keywords![0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: <Widget>[
+        Container(padding: EdgeInsets.all(20)),
+        CarouselSlider(
+          items: images,
+          options: CarouselOptions(
+              autoPlay: false,
+              onPageChanged: (index, reasone) {
+                setState(() {
+                  _currentPage = index;
+                  _currentKeyword = keywords![_currentPage];
+                });
+              }),
+        ),
+        Container(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 3),
+            child: Text(
+              _currentKeyword,
+              style: TextStyle(fontSize: 11),
+            )),
+        Container(
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                    child: Column(
+                  children: <Widget>[
+                    likes![_currentPage]
+                        ? IconButton(onPressed: () {}, icon: Icon(Icons.check))
+                        : IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+                    Text('Like content', style: TextStyle(fontSize: 11))
+                  ],
+                )),
+                Container(
+                  padding: EdgeInsets.only(right: 10),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(primary: Colors.white),
+                    onPressed: () {},
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.play_arrow, color: Colors.black),
+                        Padding(padding: EdgeInsets.all(3)),
+                        Text('Play', style: TextStyle(color: Colors.black))
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(right: 10),
+                  child: Column(children: <Widget>[
+                    IconButton(onPressed: () {}, icon: Icon(Icons.info)),
+                    Text(
+                      'info',
+                      style: TextStyle(fontSize: 11),
+                    )
+                  ]),
+                )
+              ]),
+        ),
+        Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: makeIndicator(likes!, _currentPage),
+          ),
+        )
+      ],
+    ));
+  }
+}
+
+List<Widget> makeIndicator(List<bool> list, int _currentPage) {
+  List<Widget> result = [];
+  for (int i = 0; i < list.length; ++i) {
+    result.add(Container(
+      width: 8,
+      height: 8,
+      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: _currentPage == i
+              ? Color.fromRGBO(255, 255, 255, 0.9)
+              : Color.fromRGBO(255, 255, 255, 0.4)),
+    ));
+  }
+
+  return result;
+}
